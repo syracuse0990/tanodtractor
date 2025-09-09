75384608cca62ba86f2c246f99058ba6e999eb68
alniv94 <alvinmiracastillo@gmail.com>
2025-09-10 01:06:12 +0800

    asd

--- app/Http/Api/DeviceController.php
+++ app/Http/Api/DeviceController.php
@@ -12,6 +12,7 @@ use App\Models\Tractor;
 use App\Models\TractorBooking;
 use App\Models\TractorGroup;
 use App\Models\User;
+use App\Models\TaggingHistory;
 use Exception;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Request;
